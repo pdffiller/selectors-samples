57f2f44c0345b9975eca66782074e02c382bc8bf
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2017-10-05 12:36:00 +0300

    [sample-1-7] state normalization

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,17 +1,58 @@
+import { combineReducers } from 'redux';
 import { ADD_USER } from '../actions/types';
 
-const users = (state = [], action) => {
+const initialState = {
+  id: null,
+  name: '',
+  email: '',
+  avaUrl: '',
+};
+
+const user = (state = initialState, action) => {
   switch (action.type) {
     case ADD_USER:
-      return state.concat(action.payload);
+      return { ...state, ...action.payload };
 
     default:
       return state;
   }
 };
 
-export const getUserIds = state => state.users.map(u => u.id);
-export const getUsers = state => state.users;
-export const getUsersCount = state => getUsers(state).length;
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      const { id } = action.payload;
+      return { ...state, [id]: user(state[id], action) };
+
+    default:
+      return state;
+  }
+};
+
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return [...state, action.payload.id];
+
+    default:
+      return state;
+  }
+};
+
+const users = combineReducers({ byId, allIds });
+
+
+// Selectors: ------------------------------------------------------------------
+
+const entityById = entities => id => entities[id];
+const mapIds = (ids, entities) => ids.map(entityById(entities));
+
+export const getUserIds = state => state.users.allIds;
+export const getUserEntities = state => state.users.byId;
+
+export const getUsers = state => mapIds(
+  getUserIds(state), getUserEntities(state)
+);
+export const getUsersCount = state => getUserIds(state).length;
 
 export default users;
